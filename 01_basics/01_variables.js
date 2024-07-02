718110eeeba8b7fdbe44a9ef9f976c30e4a094c2
Pratik Patel <pp301299@gmail.com>
2024-07-02 21:24:28 +0530

    01_variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = "01234";
+let accountEmail = "pratik@gmail.com";
+var accountPassword = "1234"
+let accountState;
+
+accountCity = "Gujarat";
+
+// accountId = "444444";
+ accountEmail = "rutu@gmail.com";
+ accountPassword = "0000";
+
+ /*
+   prefer not to use var 
+   because issue in block scope and function scope
+ */
+
+accountCity = "Mumbai"
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
